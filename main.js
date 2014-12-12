ddf2b6742053f118233759949228bb5ab3684e42
Vasuman R <vasumanar@gmail.com>
2014-12-12 11:35:24 +0530

    Added Presets

--- main.js
+++ main.js
@@ -75,20 +75,41 @@ function addPresets() {
         opt.triggerFunc = func;
         sI('preset-select').add(opt);
     }
-    addOption('Tree',
+    addOption('', set('', '', ''));
+    addOption('Pythogorean Tree',
         set('A : A A\nB : A X B Y B',
-        'X = DO (PUSH) (RT 45)\nY = DO (POP) (LT 45)\nA = FD 10\nB = FD 10',
-        'B')
+        'X = DO (PUSH) (RT 45)\nY = DO (POP) (LT 45)\nA = FD 10\nB = FD 10\nU = RT 180',
+        'U B')
     );
-    addOption('Koch',
+    addOption('Fractal Plant',
+        set('A : A A\nX : A L I I X J R X J R A I R A X J L X',
+        'I = PUSH\nJ = POP\nA = FD 10\nR = RT 25\nL = LT 25\nU = RT 180',
+        'U X')
+    );
+    addOption('Koch Curve',
         set('F : F A F B F B F A F',
         'F = FD 10\nA = RT 90\nB = LT 90',
-        'F')
+        'A F')
     );
-    addOption('Sierpinski triangle',
+    addOption('Sierpinski Triangle',
         set('A : B R A R B\nB : A L B L A',
         'A = FD 10\nB = FD 10\nR = RT 60\nL = LT 60',
         'A')
+    );
+    addOption('Dragon Curve',
+        set('X : X R Y F\nY : F X L Y',
+        'F = FD 10\nR = RT 90\nL = LT 90',
+        'F X')
+    );
+    addOption('Koch Snowflake',
+        set('F : F A F B F A F',
+        'F = FD 10\nA = RT 60\nB = LT 120\nR = RT 90',
+        'R F B F B F')
+    );
+    addOption('Cantor Dust',
+        set('A : A X B Y A\nB : B B B',
+        'A = FD 10\nX = COLOR white\nY = COLOR black\nB = FD 10\nR = RT 90',
+        'R A')
     );
 }
 
